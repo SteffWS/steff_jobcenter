b6b90423c1c73c74faa428a20b73d93339fc4e57
Steff <84989476+SteffWS@users.noreply.github.com>
2025-07-27 21:30:48 +0200

    added: fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Steff'
+description 'Advanced QB Job Center'
+version '1.0.0'
+
+shared_scripts {
+    '@qb-core/import.lua',
+    'locales/*.lua',
+    'config.lua'
+}
+
+client_script {
+    'client/cl_main.lua'
+}
+
+server_script {
+    'server/sv_main.lua'
+}
+
+dependencies {
+  'qb-core',
+}
